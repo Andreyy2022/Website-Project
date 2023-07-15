8524403bb1c52e95c9792be6ee68835f2c8d7b84
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-07-15 15:09:51 +0300

    website 1.2.0

--- time-root-react.js
+++ time-root-react.js
@@ -1,20 +1,3 @@
 'use strict';
 
-const root = ReactDOM.createRoot(document.getElementById('time-root'));
 
-class Clock extends React.Component {
-  render() {
-    return (
-      <span>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
-      </span>
-    );
-  }
-}
-
-function tick() {
-  root.render(<Clock date={new Date()} />);
-}
-
-setInterval(tick, 1000);
