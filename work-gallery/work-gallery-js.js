80326d403df9b93634690f8f3e0526c426a47d50
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-07-24 19:49:56 +0300

    website 1.7.6

--- work-gallery/work-gallery-js.js
+++ work-gallery/work-gallery-js.js
@@ -1,23 +1,4 @@
-<?php
-$images = array();
-$folder = 'work-gallery-images';
-$array_to_js = 'var imgs=[';
-$all_files = scandir($folder);
-for ($i=2; $i<=sizeof($all_files)-1; $i++) {
-    $array_to_js.='"'.$folder.$all_files[$i].'",';
-}
-echo substr($array_to_js,0,-1).'];';
-?>
-
-var n = 0;
-var time = 800;
-play = setInterval("chgImg()", 3000);
-
-function chgImg() {
-$('#slide_show').fadeOut(time, function() {
-  $(this).attr('src', imgs[n]).fadeIn(time);
-});
-
-n++;
-if (n>=imgs.length) n = 0;
+for (i = 1; i < 5; i++)
+{
+document.write("<img src=" + "work-gallery-images/" + i + ".jpg" + ">");
 }
